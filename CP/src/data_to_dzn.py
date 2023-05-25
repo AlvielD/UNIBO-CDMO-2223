03f667f35668a2211729282bf96d8cb19846c025
headman99 <54580381+headman99@users.noreply.github.com>
2023-05-25 17:00:53 +0200

    Update data_to_dzn.py

--- CP/src/data_to_dzn.py
+++ CP/src/data_to_dzn.py
@@ -1,27 +1,28 @@
 import os
 
+
+
 def data_to_dzn():
 
-    in_file_path = '../../data'     # Data folder on the repository
-    out_file_path = '../data'       # Data folder on the CP folder
+    in_file_path = 'C:/Users/david/UNI/Combinatorial/Progetto/CDMO-Project/data'     # Data folder on the repository
+    out_file_path = 'C:/Users\david/UNI/Combinatorial/Progetto/CDMO-Project/CP/data'       # Data folder on the CP folder
 
     n_instances = len(os.listdir(in_file_path))
 
-    for i in range(1, n_instances+1):
-        with open(f'{in_file_path}/instance_{i}.txt', 'r') as f:
-            lines = f.read().splitlines()
-
-            m = lines[0]
-            n = lines[1]
-            
-            l = [int(e) for e in lines[2].split(' ')]
-            s = [int(e) for e in lines[3].split(' ')]
-
+    for i in range(1, n_instances):
+        index = i if i>9 else f'0{i}'
+        with open(f'{in_file_path}/inst{index}.dat', 'r') as f:
+            lines = [e.split('\n')[0] for e in f.readlines()]
+            m = int(lines[0])
+            n = int(lines[1])
+            l = [int(e) for e in lines[2].split(' ')[0]]
+            s = [int(e) for e in lines[3].split(' ')[0]]
             D = []
             for line in lines[4:]:
-                D.append([int(e) for e in line.split(' ')])
+                D.append([int(p) for p in line.split(' ')[0]])
             
-        with open(f'{out_file_path}/instance_{i}.dzn', 'w') as f:
+            
+        with open(f'{out_file_path}/inst{index}.dzn', 'w') as f:
             f.write(f'm = {m};\n')
             f.write(f'n = {n};\n')
             f.write(f'l = {l};\n')
